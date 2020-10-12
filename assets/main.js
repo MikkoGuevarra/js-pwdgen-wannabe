7ca1682e8fe515c63883d3d631391848e3f2662a
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-12 15:18:23 +0200

    made my first prompt

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+var first_name = prompt("What is your First Name");
+console.log(first_name);
+var last_name = prompt("What is your Last Name");
+console.log(last_name);
+var fave_color = prompt("What is you Favourite Color?")
+console.log(fave_color);
+var fave_number = 10;
+document.getElementById('pwd-generated').innerHTML = first_name + last_name + fave_color + fave_number;
